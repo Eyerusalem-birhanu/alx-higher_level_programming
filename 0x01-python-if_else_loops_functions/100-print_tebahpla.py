cf01a7491f685b5c9f5ad2013746755e1d2a672c
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-08-24 09:46:50 -0700

    advance

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''print the alphabet in reverse order alterning upper and lowe case'''
 i = 0
-for c in range(odr('z'), odr('a') - 1, -1):
+for c in range(ord('z'), ord('a') - 1, -1):
     print('{}'.format(chr(c - i)), end='')
     i = 32 if i == 0 else 0
