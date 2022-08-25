a954886306c6125d0436b29939d02f450fa958f5
Eyerusalem-birhanu <jerrybirhanu94@gmail.com>
2022-08-25 12:16:38 +0300

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
-#!usr/bin/python3
-if __name__ == '__mani__':
-    '''print the number of and list of arguments'''
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the number of and list of arguments"""
     import sys
 
     count = len(sys.argv) - 1
     if count == 0:
-        print('0 arguments.')
+        print("0 arguments.")
     elif count == 1:
-        print('1 argument:')
+        print("1 argument:")
     else:
-        print('{} arguments:'.format(count))
-    for i in range (count):
-        print('{}: {}'.format(i + 1, sys.argn[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
